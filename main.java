441547222c9652cd30e71c5e6603102bfa0e9412
FRBF Studios <tfroempp@outlook.de>
2025-01-09 16:18:08 +0100

    Added basics to Main, capitalized file name

--- main.java
+++ main.java
@@ -1,1 +1,11 @@
+import javax.swing.*;
 
+public class Main {
+    JPanel testWindow = new JPanel();
+
+    JButton testButton = new JButton();
+    @SuppressWarnings("unused")
+    Main () {
+        testWindow.add(testButton);
+    }
+}
