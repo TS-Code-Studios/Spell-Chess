507fdf600162524dc54c4b67fb1cd3a0603317ec
FRBF Studios <tfroempp@outlook.de>
2025-01-09 16:49:28 +0100

    Added basic structure to Main.java, should create a window now

--- main.java
+++ main.java
@@ -1,11 +1,18 @@
 import javax.swing.*;
 
-public class Main {
-    JPanel testWindow = new JPanel();
+public class Main extends JFrame {
 
-    JButton testButton = new JButton();
-    @SuppressWarnings("unused")
-    Main () {
-        testWindow.add(testButton);
+    public static void createAndShowGUI() {
+        JFrame testGUI = new JFrame();
+        JButton testButton = new JButton("test");    
+        testGUI.add(testButton);
+        testGUI.setSize(500, 500);
+        testGUI.setLocationRelativeTo(null);
+        testGUI.pack();
+        testGUI.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        createAndShowGUI();
     }
 }
