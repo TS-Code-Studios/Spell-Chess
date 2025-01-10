b81523eb2f58475d0900fd0af86ad5c6e9edf2fb
FRBF Studios <124185414+FRBFStudios@users.noreply.github.com>
2025-01-10 11:29:03 +0100

    Update MainMenu.java
    Reformatted everything, changed names to match CATALOGUE.md and added comments.
    UI elements are now defined outside of the launchMainMenu function.

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,13 +1,19 @@
 import javax.swing.*;
 
-public class MainMenu extends JFrame {
-    public static void createAndShowGUI() {
-        JFrame menuGUI = new JFrame();
-        JPanel title = new JPanel("Spell Chess");    
-        menuGUI.add(title);
-        menuGUI.setSize(500, 500);
-        menuGUI.setLocationRelativeTo(null);
-        menuGUI.pack();
-        menuGUI.setVisible(true);
+public class MainMenu {
+    //Every element of the GUI is defined
+    JFrame mainMenu;
+    JPanel title;
+    /* Method for creating the main menu JFrame, making it visible,
+    creating the elements and adding the elements to the window. */
+    public static void launchMainMenu() {
+        mainMenu = new JFrame();
+        title = new JPanel("Spell Chess");
+        
+        mainMenu.add(title);
+        mainMenu.setSize(500, 500);
+        mainMenu.setLocationRelativeTo(null);
+        mainMenu.pack();
+        mainMenu.setVisible(true);
     }
 }
