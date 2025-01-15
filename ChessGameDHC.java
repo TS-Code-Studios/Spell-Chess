78975ebec7d26411a6b0f9bfa304a5601a73bb5d
FRBF Studios <124185414+FRBFStudios@users.noreply.github.com>
2025-01-15 19:53:24 +0100

    Bugfixes, compiled everything

--- ChessGameDHC.java
+++ ChessGameDHC.java
@@ -2,12 +2,18 @@
 //Version: 15/1/2024
 //See INFORMATION.md for more info
 
-public ChessGameDHC {
+public class ChessGameDHC {
   //This 2D array contains the chess position to be displayed on the board
+  @SuppressWarnings("unused")
   String[][] position;
   //This 1D array contains info such as En Passant target squares, the last move, castling availability, etc.
+  @SuppressWarnings("unused")
   String[] positionMeta;
+  
+  //Constructor
+  @SuppressWarnings("unused")
   ChessGameDHC() {
+    //position array is filled with the default chess position (Uppercase = black pieces, lowercase = white pieces)
     position = new String[][] {
       {"R", "N", "B", "Q", "K", "B", "N", "R"},
       {"P", "P", "P", "P", "P", "P", "P", "P"},
@@ -18,6 +24,8 @@ public ChessGameDHC {
       {"p", "p", "p", "p", "p", "p", "p", "p"},
       {"r", "n", "b", "q", "k", "b", "n", "r"}
     };
+    
+    //positionMeta array is filled with the default chess position's meta
     //Order: white O-O, white O-O-O, black O-O, black O-O-O, en passant square, halfmoves, check
     positionMeta = new String[] {"-", "-", "-", "-", "-", "-", "-"};
   }
--- src/gui/ChessboardGUI.java
+++ src/gui/ChessboardGUI.java
@@ -1,14 +1,15 @@
+package gui;
+
 //Developer: @FRBF Studios
 //Version: 15/1/2024
 //For more information check out INFORMATION.md
 
-import javax.swing;
-import java.awt.ActionEvent;
+import javax.swing.*;
 
 public class ChessboardGUI {
   JFrame chessboardWindow;
   public ChessboardGUI () {
-    chessboardWindow = new JFrame;
-    chessboardWindow.setVisible = True;
+    chessboardWindow = new JFrame();
+    chessboardWindow.setVisible(true);
   }
 }
