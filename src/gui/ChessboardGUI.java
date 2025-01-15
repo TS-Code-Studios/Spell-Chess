4207da80f8171d7c49b83114a886884fa37b21e8
FRBF Studios <124185414+FRBFStudios@users.noreply.github.com>
2025-01-15 09:52:07 +0100

    Create ChessboardGUI.java
    See INFORMATION.md

---
+++ src/gui/ChessboardGUI.java
@@ -0,0 +1,14 @@
+//Developer: @FRBF Studios
+//Version: 15/1/2024
+//For more information check out INFORMATION.md
+
+import javax.swing;
+import java.awt.ActionEvent;
+
+public class ChessboardGUI {
+  JFrame chessboardWindow;
+  public ChessboardGUI () {
+    chessboardWindow = new JFrame;
+    chessboardWindow.setVisible = True;
+  }
+}
