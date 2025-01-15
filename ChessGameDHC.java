2b7f68dc5d6d05b2525427a73451f620b0139071
FRBF Studios <124185414+FRBFStudios@users.noreply.github.com>
2025-01-15 11:30:41 +0100

    Update and rename src/ChessHandlerDHC.java to ChessGameDHC.java
    Renamed, also added positionMeta and position 2D arrays

---
+++ ChessGameDHC.java
@@ -0,0 +1,23 @@
+//Author: @FRBFStudios
+//Version: 15/1/2024
+//See INFORMATION.md for more info
+
+public ChessGameDHC {
+  //This 2D array contains the chess position to be displayed on the board
+  String[][] position;
+  //This 1D array contains info such as En Passant target squares, the last move, castling availability, etc.
+  String[] positionMeta;
+  ChessGameDHC() {
+    position = new String[][] {
+      {"R", "N", "B", "Q", "K", "B", "N", "R"},
+      {"P", "P", "P", "P", "P", "P", "P", "P"},
+      {"-", "-", "-", "-", "-", "-", "-", "-"},
+      {"-", "-", "-", "-", "-", "-", "-", "-"},
+      {"-", "-", "-", "-", "-", "-", "-", "-"},
+      {"-", "-", "-", "-", "-", "-", "-", "-"},
+      {"p", "p", "p", "p", "p", "p", "p", "p"},
+      {"r", "n", "b", "q", "k", "b", "n", "r"}
+    };
+    positionMeta = new String[] {};
+  }
+}
