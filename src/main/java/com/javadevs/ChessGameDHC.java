8b44e1a7cbbce89158f3b740a4d52d2f542dbaf6
FRBF Studios <124185414+FRBFStudios@users.noreply.github.com>
2025-01-16 19:18:07 +0100

    Added some new methods and pawn move recognition

--- src/main/java/com/javadevs/ChessGameDHC.java
+++ src/main/java/com/javadevs/ChessGameDHC.java
@@ -8,7 +8,7 @@ public class ChessGameDHC {
   String[][] position;
   //This 1D array contains info such as En Passant target squares, the last move, castling availability, etc.
   String[] positionMeta;
-  
+
   //Constructor
   @SuppressWarnings("unused")
   ChessGameDHC() {
@@ -36,4 +36,68 @@ public class ChessGameDHC {
   public String[] getPositionMeta() {
     return positionMeta;
   }
+
+  //Checks whether or not two pieces are of the same color
+  public boolean isPieceSameColor(String piece1, String piece2) {
+    boolean isOfSameColor = false;
+
+    if (Character.isUpperCase(piece1.charAt(0)) == Character.isUpperCase(piece2.charAt(0))) {
+      isOfSameColor = true;
+    }
+
+    return isOfSameColor;
+  }
+  
+  //This method can check if a move is possible. So far, pins, promotions and en passant are not included. For playerToMove, use "w" and "B" respectively.
+  public boolean isMovePossible(String piece, String startSquare, String targetSquare, String playerToMove) {
+    boolean isMovePossible = false;
+    
+    //The startSquare string is seperated
+    int startSquareFile = startSquare.charAt(0) - 'A' + 1;
+    int startSquareRank = startSquare.charAt(1);
+
+    //The targetSquare string is seperated
+    int targetSquareFile = targetSquare.charAt(0) - 'A' + 1;
+    int targetSquareRank = targetSquare.charAt(1);
+
+    //Checks if it's the right player to move
+    if (isPieceSameColor(piece, playerToMove)) {
+      //Checks if the target square exists to avoid crashes
+      if (targetSquareRank > 0 && targetSquareRank < 9 && targetSquareFile > 0 && targetSquareFile < 9) {
+        //Is the piece a black pawn?
+        if ("P".equals(piece)) {
+          //Is the pawn staying on its file? Is the target square empty?
+          if (targetSquareFile == startSquareFile && "-".equals(position[targetSquareFile][targetSquareRank])) {
+            //Is it moving 1 square downwards?
+            if (startSquareRank-- == targetSquareRank) {
+              isMovePossible = true;
+              //Is it moving 2 squares downwards from the 7th rank?
+            } else if (startSquareRank == 7 && (startSquareRank - 2) == targetSquareRank) {
+              isMovePossible = true;
+            }
+            //Is the pawn capturing a piece? (^ = XOR)
+          } else if (startSquareFile-- == targetSquareFile ^ startSquareFile++ == targetSquareFile && startSquareRank-- == targetSquareRank && !isPieceSameColor("P", position[targetSquareFile][targetSquareRank])) {
+            isMovePossible = true;
+          }
+          //Is the piece a white pawn?
+        } else if ("p".equals(piece)) {
+          //Is the pawn staying on its file? Is the target square empty?
+          if (targetSquareFile == startSquareFile && "-".equals(position[targetSquareFile][targetSquareRank])) {
+            //Is it moving 1 square upwards?
+            if (startSquareRank++ == targetSquareRank) {
+              isMovePossible = true;
+              //Is it moving 2 squares upwards from the 2nd rank?
+            } else if (startSquareRank == 2 && (startSquareRank + 2) == targetSquareRank) {
+              isMovePossible = true;
+            }
+            //Is the pawn capturing a piece? (^ = XOR)
+          } else if (startSquareFile-- == targetSquareFile ^ startSquareFile++ == targetSquareFile && startSquareRank++ == targetSquareRank && !isPieceSameColor("P", position[targetSquareFile][targetSquareRank])) {
+            isMovePossible = true;
+          }
+        }
+      }
+    }
+    
+    return isMovePossible;
+  };
 }
