08c3b9692a9cc7b614aeac8085c68a9ec378902d
simonkdev <simonkorten09@gmail.com>
2025-02-17 11:31:29 +0000

    Update ChessboardGUI.java

--- src/main/java/com/javadevs/gui/ChessboardGUI.java
+++ src/main/java/com/javadevs/gui/ChessboardGUI.java
@@ -1,21 +1,23 @@
 package com.javadevs.gui;
 
-//Developer: @FRBF Studios
-//Version: 15/1/2024
+//Developer: @simonkdev
+//Version: 17/02/2025
 //For more information check out INFORMATION.md
 
 import javax.swing.JFrame;
 
 import com.javadevs.ChessGameHandler;
 
-public class ChessboardGUI {
+public class ChessboardGUI 
+{
   @SuppressWarnings("unused")
   ChessGameHandler game = new ChessGameHandler();
-  JFrame chessboardWindow;
+ 	JFrame chessboardWindow;
+		JButton 
 
-
-  public ChessboardGUI () {
+  public ChessboardGUI () 
+		{
     chessboardWindow = new JFrame();
     chessboardWindow.setVisible(true);
-  }
+  	}
 }
