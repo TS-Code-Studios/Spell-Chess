c019a091620c79a988a1163f5d3619a0248ee87e
simonkdev <simonkorten09@gmail.com>
2025-03-10 20:02:04 +0100

    big upgred

--- src/main/java/com/javadevs/gui/ChessboardGUI.java
+++ src/main/java/com/javadevs/gui/ChessboardGUI.java
@@ -4,10 +4,12 @@ package com.javadevs.gui;
 //Version: 17/02/2025
 //For more information check out INFORMATION.md
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JFrame;
 
 import com.javadevs.ChessGameHandler;
-import com.javadevs.ChessButton;
 
 //für position[][] gilt: position[y][x] (im Array)
 
@@ -15,28 +17,35 @@ public class ChessboardGUI
 {
   @SuppressWarnings("unused")
   ChessGameHandler game = new ChessGameHandler();
- 	 JFrame chessboardWindow;
-	 
+  JFrame chessboardWindow;
+  List<chessButton> buttons;
 
   public ChessboardGUI () 
-		{
+  {
     chessboardWindow = new JFrame();
     chessboardWindow.setVisible(true);
-  	}
-	
-	public buttonInit()
-	{
-			int count = 0;
-			String countString = "";
-		
-			for(int i=0, i<16, i++)
-			{
-					countString = i;
-					new ChessButton(countString);
-			}
-	
-	
+    buttons = new ArrayList<>();
+  }
+    
+  @SuppressWarnings("static-access") 						//Otherwise causes trouble in buttonInit() for using setSquareColor()
+public void buttonInit()
+  {
+    int count = 0;
+    int countA = 1;
 
+    //creates buttons for column a
+    for(int i=0; i<8; i++)
+    {
+      chessButton button = new chessButton("a", String.valueOf(countA));
+      buttons.add(button);
+      countA++;
+      count++;
+    }
 
-		
+    // Call setSquareColor on each button
+    for (chessButton button : buttons) {
+      button.setSquareColor();
+    }
+  }
 }
+
